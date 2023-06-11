2edff1d7ed05c666877642dbe4aaa04b4c74ca0a
kaio pomini <kaiopomini@gmail.com>
2023-06-11 18:02:21 -0300

    teste

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const Menu: React.FC = () => {
-  return <Container>Menu</Container>;
+  return <Container>Menu teste</Container>;
 };
 
 export default Menu;

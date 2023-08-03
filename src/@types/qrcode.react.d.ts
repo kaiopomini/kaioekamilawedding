3b00c57697a4a605fc24cc0e9b70c4dcf92fe6fd
kaio pomini <kaiopomini@gmail.com>
2023-08-03 09:51:50 -0300

    pix

--- src/@types/qrcode.react.d.ts
+++ src/@types/qrcode.react.d.ts
@@ -10,34 +10,34 @@
 
 declare namespace qrcode {
   interface ImageSettings {
-      src: string;
-      x?: number | undefined;
-      y?: number | undefined;
-      height?: number | undefined;
-      width?: number | undefined;
-      excavate?: boolean | undefined;
+    src: string;
+    x?: number | undefined;
+    y?: number | undefined;
+    height?: number | undefined;
+    width?: number | undefined;
+    excavate?: boolean | undefined;
   }
 
   interface BaseQRCodeProps {
-      value: string;
-      size?: number | undefined;
-      includeMargin?: boolean | undefined;
-      bgColor?: string | undefined;
-      fgColor?: string | undefined;
-      level?: "L"|"M"|"Q"|"H" | undefined;
-      imageSettings?: ImageSettings | undefined;
+    value: string;
+    size?: number | undefined;
+    includeMargin?: boolean | undefined;
+    bgColor?: string | undefined;
+    fgColor?: string | undefined;
+    level?: 'L' | 'M' | 'Q' | 'H' | undefined;
+    imageSettings?: ImageSettings | undefined;
   }
 
   type CanvasQRCodeProps = BaseQRCodeProps & {
-      renderAs?: "canvas" | undefined
+    renderAs?: 'canvas' | undefined;
   } & React.CanvasHTMLAttributes<HTMLCanvasElement>;
 
   type SvgQRCodeProps = BaseQRCodeProps & {
-      renderAs: "svg"
+    renderAs: 'svg';
   } & React.SVGProps<SVGSVGElement>;
 
   type QRCode = React.ComponentClass<CanvasQRCodeProps | SvgQRCodeProps>;
 }
 
 declare const qrcode: qrcode.QRCode;
-export = qrcode
+export = qrcode;
